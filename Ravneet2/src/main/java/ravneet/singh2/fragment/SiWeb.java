a6dcdfa2c81f2b16df5b3be19e2dbc35c9d2809a
Guru Sharma <n01142131@humber.ca>
2018-01-07 05:11:07 -0500

    d

--- Ravneet2/src/main/java/ravneet/singh2/fragment/SiWeb.java
+++ Ravneet2/src/main/java/ravneet/singh2/fragment/SiWeb.java
@@ -1,12 +1,26 @@
 package ravneet.singh2.fragment;
 
 import android.content.Context;
+import android.location.Address;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import ravneet.singh2.R;
 
@@ -15,12 +29,63 @@ import ravneet.singh2.R;
 //Assignment 2
 
 public class SiWeb extends Fragment {
+    TextView cityTV, stateTV, codeTV;
+    Button mButton;
+    private RequestQueue requestQueue;
+    EditText mEditText;
     View mView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.si_web, container, false);
+
+        requestQueue = Volley.newRequestQueue(getActivity());
+        cityTV = mView.findViewById(R.id.city_tv);
+        stateTV = mView.findViewById(R.id.state_tv);
+        codeTV = mView.findViewById(R.id.zipcode_tv);
+        mButton = mView.findViewById(R.id.webservice_btn);
+        mEditText = mView.findViewById(R.id.zip_et);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService();
+            }
+
+            private void startService() {
+                AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        JsonObjectRequest request = new JsonObjectRequest("http://api.zippopotam.us/us/95210" +
+                                mEditText.getText().toString(),
+                                new Response.Listener<JSONObject>(){
+
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+
+                                        String location = null;
+                                        try {
+                                            location = response.getJSONArray("results").getJSONObject(0).getString("formatted_address");
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+
+
+                                    }
+                                }, new Response.ErrorListener(){
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+                        requestQueue.add(request);
+
+                    }
+                });
+            }
+        });
+
         return mView;
     }
 }
